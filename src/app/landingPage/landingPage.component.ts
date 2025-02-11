10c802eb878d5ffce62a1ed8e58d0657a95896b1
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-11 13:12:47 +0100

    landingPage component added

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,16 @@
+
+import { Component } from '@angular/core';  //aus der app.component.ts kopiert
+import { RouterOutlet } from '@angular/router'; //@ bezeichnet Pfad
+
+@Component({        //decorater, funktionalität rum herum die Klasse
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [],    //wenn wir etwas importieren möchten, Kevin tut cCommonModule rein, den habe ich nicht
+    template: `<section>
+        <h1>SAKURA RAMEN</h1>
+        <h4>THE BEST RAMEN IN TOWN</h4>
+        </section>`,   //wir können auch bloß url eingeben als templateUrls (wird angeboten)
+    styleUrls: ['./landingPage.component.scss']
+})
+
+export class LandingPageComponent {} //hier kommt mein ts code (JS) rein

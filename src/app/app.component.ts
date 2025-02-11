d6f2f46d1ace821ec0e1e3708e37d412c7f82cf9
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-11 17:26:31 +0100

    second try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from "./main-content/main-content.component";
 import { OurMenuComponent } from './main-content/our-menu/our-menu.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContentComponent, OurMenuComponent],
+  imports: [RouterOutlet, MainContentComponent, OurMenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 
 @Component({
   selector: 'app-main-content',
-  imports: [LandingPageComponent, OurMenuComponent],
+  imports: [LandingPageComponent, OurMenuComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

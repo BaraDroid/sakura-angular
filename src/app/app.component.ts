56ad4124f80938e4ba4bf9a6d4bfda6ebb2204dd
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-12 10:38:00 +0100

    changes in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from "./main-content/main-content.component";
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { LandingPageComponent } from './main-content/landingPage/landingPage.component';
-import { ImprintComponent } from './imprint/imprint.component';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPageComponent, MainContentComponent, FooterComponent],
+  imports: [RouterOutlet, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
